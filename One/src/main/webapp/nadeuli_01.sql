23a6f4fa217ce60f71955e39a5cbb22bb2f68b77
smhrd <smhrd@DESKTOP-5RJ7E19>
2021-08-06 15:59:43 +0900

    index-2에서 안심여행지 누르면 현욱오빠가 만든부분으로 넘어가게 수정함.

--- One/src/main/webapp/nadeuli_01.sql
+++ One/src/main/webapp/nadeuli_01.sql
@@ -16,11 +16,12 @@ select * from MEMBERS
 select email from members where email='test1'
 
 CREATE TABLE city (
-   city_name varchar2(10) NOT NULL,
-   c_introduce varchar2(100) not NULL,
-   c_img varchar2(500) not null,
+   city_name varchar2(20) NOT NULL,
+   c_introduce varchar2(100),
+   c_img varchar2(1000),
    constraint city_pk primary key(city_name)
 );
+
 
 select * from CITY
 
@@ -30,17 +31,19 @@ create table community(
 	contents varchar2(500) not null,
 	commu_date date not null,
 	img_rink varchar2(500) null,
-	city_name varchar2(10) not null,
+	city_name varchar2(20) not null,
 	email varchar2(50) not null,
 	constraint commu_city_fk foreign key(city_name) references city(city_name),
 	constraint commu_email_fk foreign key(email) references members(email)
 );
 
+	-- insert into community values(comm_seq.nextval, 'test','1','테스트입니다.',sysdate);
+
 select * from community
 
 create table member_trip(
 	email varchar2(50) NOT NULL,
-	city_name varchar2(10) not null,
+	city_name varchar2(20) not null,
 	constraint trip_city_fk foreign key(city_name) references city(city_name),
 	constraint trip_email_fk foreign key(email) references members(email)
 )
@@ -49,7 +52,7 @@ select * from member_trip
 
 create table sns_data(
 	data_number number(10) not null constraint sns_pk primary key,
-	city_name varchar2(10) not null,
+	city_name varchar2(20) not null,
 	tags varchar2(500) not null,
 	sns_date varchar2(10) not null,
 	constraint sns_city_fk foreign key(city_name) references city(city_name)
@@ -72,19 +75,19 @@ create table place (
 	p_tel varchar2(20),
 	p_introduce varchar2(20) not null,
 	p_img varchar2(500) not null,
-	city_name varchar2(10) not null,
+	city_name varchar2(20) not null,
 	constraint p_fk foreign key(city_name) references city(city_name)
 )
 
 select * from place
 
 create table domitory (
-	d_name varchar2(10) not null constraint d_pk primary key,
-	city_name varchar2(10) not null,
-	d_address varchar2(20) not null,
-	d_tel varchar2(10),
-	d_type varchar2(10) not null,
-	d_url varchar2(30),
+	d_name varchar2(50) not null constraint d_pk primary key,
+	city_name varchar2(20) not null,
+	d_address varchar2(100) not null,
+	d_tel varchar2(20),
+	d_type varchar2(20) not null,
+	d_url varchar2(100),
 	constraint d_fk foreign key(city_name) references city(city_name)
 )
 
@@ -92,19 +95,20 @@ select * from domitory
 
 create table restaurant(
 	r_number number(10) not null constraint r_pk primary key,
-	r_name varchar2(20) not null,
-	r_address varchar2(20) not null,
-	r_tel varchar2(10),
-	r_type varchar2(10) not null,
-	city_name varchar2(10) not null,
+	city_name varchar2(20) not null,
+	r_name varchar2(100) not null,
+	r_address varchar2(100) not null,
+	r_tel varchar2(20),
+	r_type varchar2(30) not null,
 	constraint r_fk foreign key(city_name) references city(city_name)
 )
+
 
 commit
 
 create table corona(
 	c_date varchar2(10) not null constraint c_pk primary key,
-	city_name varchar2(10) not null,
+	city_name varchar2(20) not null,
 	confirmed_number number(5) not null,
 )
 
